b0b62ec8331881091081172d7c38f9187b42aba2
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-02-26 18:47:49 +0100

    skeleton site
    add color background & tilte

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,24 +1,43 @@
 <template>
 
+  <div>
+
+    <v-toolbar
+            dark
+
+            src=""
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Astroneer</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+
   <v-app>
     <v-content>
       <HelloWorld/>
     </v-content>
   </v-app>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
+  export default {
+    components: {
+      HelloWorld
+    },
+    data: () => ({
 
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
--- myapp/src/components/HelloWorld.vue
+++ myapp/src/components/HelloWorld.vue
@@ -1,7 +1,5 @@
 <template>
-
-    
-
+    <div>
     <v-carousel>
         <v-carousel-item
                 v-for="(item,i) in items"
@@ -9,9 +7,15 @@
                 :src="item.src"
                 reverse-transition="fade-transition"
                 transition="fade-transition"
+                style="height: auto"
         ></v-carousel-item>
-    </v-carousel>
-
+    </v-carousel> <br><br>
+    <h1 class="text-center font-weight-black font-size:70px">Astroneer</h1> <br><br><br><br><br>
+    <h2 class="text-center">Qu'est-ce que c'est ?</h2><br><br>
+    <h3 class="text-center">Astroneer est un jeu vidéo de type bac à sable sorti sur PC <br>
+        le 16 décembre 2016 en pré-alpha sur Steam, et créé par System Era Softworks. <br>
+        Une version finalisée est distribuée le 6 février 2019.</h3><br><br>
+    </div>
 
 </template>
 
