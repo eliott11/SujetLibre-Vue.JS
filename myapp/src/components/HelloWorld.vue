3cdc19118cc86a9d6d8576d7548b85cc5a1be47c
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-02-28 18:43:15 +0100

    button + txt JS +favicon

--- myapp/src/components/HelloWorld.vue
+++ myapp/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-    <v-carousel height="575px">
+
+    <v-carousel height="580px">
         <v-carousel-item
                 v-for="(item,i) in items"
                 :key="i"
@@ -10,12 +11,9 @@
                 style="height: auto"
         ></v-carousel-item>
     </v-carousel>
-    <h1 class="text-center font-weight-black" style="font-size:75px"><u>Astroneer</u></h1> <br><br><br>
-    <h2 class="text-center" style="font-size:35px"><u>Qu'est-ce que c'est ?</u></h2><br><br>
-    <h3 class="text-center" style="font-size:25px">Astroneer est un jeu vidéo de type bac à sable sorti sur PC <br>
-        le 16 décembre 2016 en pré-alpha sur Steam, et créé par System Era Softworks. <br>
-        Une version finalisée est distribuée le 6 février 2019. (source : wiki)</h3><br><br>
-
+    <h1 class="text-center font-weight-black" style="font-size:75px"><u>{{text}}</u></h1> <br><br>
+        <h2 class="text-center" style="font-size:35px"><u>{{text1}}</u></h2><br>
+    <h3 class="text-center" style="font-size:25px">{{text2}}</h3><br><br>
     </div>
 
 </template>
@@ -24,6 +22,10 @@
     export default {
         data () {
             return {
+                text : "Astroneer",
+                text1 : "Qu'est-ce que c'est ?",
+                text2 : "Astroneer est un jeu vidéo de type bac à sable sorti sur PC le 16 décembre 2016 en pré-alpha sur Steam, et créé par System Era Softworks. Une version finalisée est distribuée le 6 février 2019. (source : wiki)",
+
                 items: [
                     {
                         src: 'https://i.imgur.com/mRV9kjI.png',
--- myapp/src/components/MyFooter.vue
+++ myapp/src/components/MyFooter.vue
@@ -1,5 +1,59 @@
 <template>
     <div>
+
+            <v-row justify="center">
+                <v-dialog v-model="dialog" persistent max-width="600px">
+                    <template v-slot:activator="{ on }">
+                        <v-btn color="primary" dark v-on="on">Intéressé ?</v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">Vos informations</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field label="Nom*" required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field
+                                                label="Prénom*"
+                                                persistent-hint
+                                                required
+                                        ></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field label="Email*" required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-select
+                                                :items="['0-17', '18-29', '30-54', '54+']"
+                                                label="Age*"
+                                                required
+                                        ></v-select>
+                                    </v-col>
+                                    <v-col cols="12" sm="6">
+                                        <v-autocomplete
+                                                :items="['Prix', 'Date', 'Evenements', 'Infos du jeu']"
+                                                label="Motif*"
+                                                required
+                                                multiple
+                                        ></v-autocomplete>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                            <small>*Obligatoire</small>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="dialog = false">Fermer</v-btn>
+                            <v-btn color="blue darken-1" text onclick='window.location.reload(false)'>Envoyer</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
+            </v-row><br>
+
         <v-footer padless>
             <v-col
                     class="text-center"
@@ -8,13 +62,14 @@
                 {{ new Date().getFullYear() }} — <strong>ASTRONEER</strong>
             </v-col>
         </v-footer>
+
 </div>
 </template>
 
 <script>
     export default {
         data: () => ({
-
+            dialog: false,
         }),
     }
 </script>
